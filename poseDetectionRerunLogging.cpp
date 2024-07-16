05d1029d8e9e961dd09f4724baca4880873b95c7
Zakareeyah <zakareeyahh@gmail.com>
2024-07-16 12:36:35 -0400

    Test logging pose detection with rerun.io (problem: rerun calc poses to be further away than they actually are; may be due to opencv ver...distance more accurate when using opencv 4.6 [still have to verify that the ver is the issue and not something else])

---
+++ poseDetectionRerunLogging.cpp
@@ -0,0 +1,54 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/aruco.hpp>
+
+std::string imgPath = "/home/zakareeyah/CLionProjects/Dev/media/OAK_DICT_4x4_50_1080p.jpeg";
+std::string outPath = "/home/zakareeyah/CLionProjects/Dev/media/out_OAK_DICT_4x4_50_1080p_pose.png";
+auto dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
+
+
+cv::Mat intrinsicsMatrix = (cv::Mat_<float>(3,3) <<
+    3075.7952, 0, 1927.995,
+    0, 3075.7952, 1078.1343,
+    0, 0, 1);
+cv::Mat distCoeffs = (cv::Mat_<float>(1, 14) << 2.0888574, -82.303825, -0.00071347022, 0.0020022474, 315.66144,
+    1.8588818, -80.083954, 308.98071, 0, 0, 0, 0, 0, 0);
+
+
+void detectPose () {
+    cv::Mat image = cv::imread(imgPath, cv::IMREAD_COLOR);
+    if(!image.data) {
+        std::cout << "Could not find image " + imgPath;
+        exit(1);
+    }
+
+    // set coordinate system
+    float markerLength = 0.15;
+    cv::Mat objPoints(4, 1, CV_32FC3);
+    objPoints.ptr<cv::Vec3f>(0)[0] = cv::Vec3f(-markerLength/2.f, markerLength/2.f, 0);
+    objPoints.ptr<cv::Vec3f>(0)[1] = cv::Vec3f(markerLength/2.f, markerLength/2.f, 0);
+    objPoints.ptr<cv::Vec3f>(0)[2] = cv::Vec3f(markerLength/2.f, -markerLength/2.f, 0);
+    objPoints.ptr<cv::Vec3f>(0)[3] = cv::Vec3f(-markerLength/2.f, -markerLength/2.f, 0);
+
+
+
+    std::vector<int> ids;
+    std::vector<std::vector<cv::Point2f> > corners, rejected;
+
+    // detect markers and estimate pose
+    cv::aruco::detectMarkers(image, dictionary, corners, ids);
+
+    size_t nMarkers = corners.size();
+    std::vector<cv::Vec3d> rvecs(nMarkers), tvecs(nMarkers);
+
+    if(!ids.empty()) {
+        // Calculate pose for each marker
+        for (size_t i = 0; i < nMarkers; i++) {
+            cv::solvePnP(objPoints, corners.at(i), intrinsicsMatrix, distCoeffs, rvecs.at(i), tvecs.at(i));
+        }
+    }
+
+
+
+}

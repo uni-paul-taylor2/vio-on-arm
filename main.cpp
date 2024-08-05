621d50dd739565e41a94cf2987b23827b5027c26
Zakareeyah <zakareeyahh@gmail.com>
2024-08-04 20:17:16 -0400

    Make notes on results of experimenting with the gtsam slam example by varying the number of poses and/or landamrks

--- main.cpp
+++ main.cpp
@@ -7,13 +7,31 @@
 
 #include "VSLAM.h"
 #include "slamProto.cpp"
-
+#include "GTSAMviSAMExample2.cpp"
+#include "selectiveDataTest.cpp"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
-    slam();
+    // prototype::slam();
+    // detectPoseVid();
+    /*
+     * Defaults:
+     *  8 (camera) poses
+     *  8 landmarks
+     *
+     * Each landmark NEEDS to be seen at least TWICE before putting into ISAM else gtsam::IndeterminantLinearSystemException thrown near landmark variable
+     *      i.e.: initial estimate and at least 2 projection factors
+     * Above implies at least 2 poses are needed (with the same landmarks)
+     * At least 5 landmarks are needed else gtsam::IndeterminantLinearSystemException thrown near pose variable
+     * Increasing the number poses to 20 also worked (safe to assume on upper limit on poses?)
+     */
+    adj(20, -1);
 
+    // selective_data::createPoints();
+    // selective_data::createPoses();
+    std::cout << "====================================SLAM+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++";
+    // selective_data::slam();
 
     std::cout << "END" << std::endl;
     return 0;
